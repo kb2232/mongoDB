d3e8d717f34193bd5b4777ad0f427f4f0de68387
kunle babatunde <kb2232@nyu.edu>
2018-08-15 18:17:31 -0400

    test ready for execution

--- project1/test/create_test.js
+++ project1/test/create_test.js
@@ -4,17 +4,26 @@ const assert = require('assert');
 // according to the schema - User can only have name property and must be string;
 const User = require('../src/user');
 describe('Creating records',()=>{
-  it('saves a uer',()=>{
+  it('saves a user',(done)=>{
     //creating an instance of the User model.
     const newUser = new User({
       name: "Kunle"
     });
     //to save it into our innternal database - use save();
-    newUser.save();
+        //save() returns a promise bc it's asynchronouse
+    //we now want to assert that newUser is actually new;
+    newUser.save()
+    .then(()=>{
+      assert(!newUser.isNew);
+      //we call done to say it can run next text;
+      done();
+    });
   });
 })
 /*
 Any time you run 
 >> npm run test
 a new instance with internal unique id will be created.
+
+To prevent this, we write a "hook". Check the test_helper.js
 */
--- project1/test/test_helper.js
+++ project1/test/test_helper.js
@@ -1,8 +1,14 @@
 //bring in mongoose
 const mongoose = require('mongoose');
-//connect to mongo db from your laptop(localhost) - user_test is the name of one of our internal database.
+
+//to get rid of the warning about deprecating...we need ES6 implementation of promises;
+mongoose.Promise=global.Promise;
+
+
+//connect to mongo db from your laptop(localhost) - usertest is the name of one of our internal database.
 // we do not need to create a database like in SQL. we just make one of the internal databases.
-//we then add collections to it.
+//we then add collections to it
+
 mongoose.connect('mongodb://localhost/usertest');
 //we connect to the internal database. Once it is done attempting to connect it return promise.
 //we catch the event handlers with .once(it went well), and .on(it didnt go well);
@@ -14,3 +20,15 @@ mongoose.connection
   .on('error',(error)=>{
     console.warn('Warning',error);
   });
+
+//the "HOOK" below will run before each test;
+//this will delete any collections before we run test.
+//we need to make sure that mocha understands to run beforeEach before //running our test.
+//caling done tells mocha it can run next text;
+
+beforeEach((done)=>{
+  mongoose.connection.collections.userscollections.drop(()=>{
+      done();
+  });
+
+});
